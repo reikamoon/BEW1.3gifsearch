c9910c435cc59cb475dd0ca15896dde6b23abb56
Anjelica <ambrosio.anjelica@gmail.com>
2020-04-02 22:56:55 -0700

    added working gif search

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 // Require Libraries
 const express = require('express');
 
+// Require tenorjs near the top of the file
+  const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "3ANB6MAOXD6F", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+// Tenor.Trending.GIFs("LIMIT HERE")
+Tenor.Trending.GIFs("2").then(Results => {
+      Results.forEach(Post => {
+            console.log(`Item #${Post.id} (${Post.created}) @ ${Post.url}`);
+      });
+}).catch(console.error);
+
 // App Setup
 const app = express();
 
@@ -12,9 +26,20 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    console.log(req.query) 
-   res.render('home')
- })
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
+  })
 app.get('/greetings/:name', (req, res) => {
   // grab the name from the path provided
   const name = req.params.name;
